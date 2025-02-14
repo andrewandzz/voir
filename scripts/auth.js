968978e72cf0e5cafe7ef291e26a1310060d36a1
Andrii Dashkov <andrii.dashkov@hneu.net>
2025-02-14 06:49:04 +0200

    Try hardcoded redirect URL

--- scripts/auth.js
+++ scripts/auth.js
@@ -8,7 +8,7 @@ window.addEventListener("load", () => {
   if (!accessTokenFromUrl && !sessionStorage.getItem(ACCESS_TOKEN_KEY)) {
     const clientId =
       "1052099757196-3001d3kp6uljo616vag80a4qi59cupg1.apps.googleusercontent.com";
-    const redirectUri = window.location.origin;
+    const redirectUri = 'https://andrewandzz.github.io/voir';
     const scope = "https://www.googleapis.com/auth/spreadsheets";
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=token&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(
       scope
